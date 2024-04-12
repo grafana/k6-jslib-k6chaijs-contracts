d47e3070f192f3e23c877764e95ce9810c6382af
Caline Patriota <caline.patriota@MacBook-Pro-3.local>
2024-04-12 15:04:31 -0300

    Fix error check

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import Ajv from 'ajv';
 
+function check(condition, message) {
+  if (!condition) {
+    throw new Error('\n' + '           '+  message);
+  }
+}
+
 export function initContractPlugin(chai){
   var _ajv = new Ajv();
 
   chai.util.addMethod(chai.Assertion.prototype, 'matchSchema', function (schema) {
     var data = chai.util.flag(this, 'object');
-  
     let validate = _ajv.compile(schema);
-  
+
     let is_valid = validate(data, schema);
-  
     // optional. It records specific error messages as checks.
     if (!is_valid) {
       console.error(JSON.stringify(validate.errors));
-  
       validate.errors.forEach(error => {
-        check(is_valid, {
-          [`${error.dataPath} ${error.message}`]: (is_valid) => is_valid
-        });
+        check(is_valid, `${error.dataPath} ${error.message}`);
       });
     }
-  
     this.assert(
       is_valid
     , "expected to match API schema"
@@ -29,6 +29,4 @@ export function initContractPlugin(chai){
     , null   // actual
     );
   });
-  
-
 }
